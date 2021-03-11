e0442e9f86abe46b0edddf247eb00f23bf9ae884
Alexey Tretyakov <atretiakov@tourmalinecore.com>
2021-03-11 16:14:58 +0500

    chore(react-table-responsive): update story

--- packages/react-table-responsive/src/stories/1-Table.stories.js
+++ packages/react-table-responsive/src/stories/1-Table.stories.js
@@ -69,51 +69,53 @@ ClientSideMobile.parameters = {
 const data = [
   {
     actualSales: 5096606.9,
-    bonusObject: 'CARDIO',
-    bonusObjectType: 'BusinessUnit',
+    employee: 'Employee 1',
     calculatedBonus: 2774.4,
     plannedSales: 5593097.36,
     previousSales: 4354489.83,
     targetAchivementPercent: 91.12,
     weight: 25,
-    weightForSorting: '025,00-CARDIO',
     someType: someTypes.firstType,
   },
   {
     actualSales: 21455221.56,
-    bonusObject: 'CNS',
-    bonusObjectType: 'BusinessUnit',
+    employee: 'Employee 2',
     calculatedBonus: 7507.2,
     plannedSales: 23126748.43,
     previousSales: 19740627.1,
     targetAchivementPercent: 92.77,
-    weight: 25,
-    weightForSorting: '025,00-CNS',
+    weight: 32,
     someType: someTypes.secondType,
   },
   {
     actualSales: 7709519.93,
-    bonusObject: 'GYNO',
-    bonusObjectType: 'BusinessUnit',
+    employee: 'Employee 3',
     calculatedBonus: 36755.2,
     plannedSales: 7385674.05,
     previousSales: 6572816.8,
     targetAchivementPercent: 104.38,
-    weight: 25,
-    weightForSorting: '025,00-GYNO',
+    weight: 12,
     someType: someTypes.firstType,
   },
   {
     actualSales: 12931847.81,
-    bonusObject: 'OTC',
-    bonusObjectType: 'BusinessUnit',
+    employee: 'Employee 4',
     calculatedBonus: 36480,
     plannedSales: 12424437.43,
     previousSales: 11570609.21,
     targetAchivementPercent: 104.08,
-    weight: 25,
-    weightForSorting: '025,00-OTC',
+    weight: 54,
     someType: someTypes.secondType,
+  },
+  {
+    actualSales: 14931847,
+    employee: 'Employee 5',
+    calculatedBonus: 66480,
+    plannedSales: 1242437.43,
+    previousSales: 1570609.21,
+    targetAchivementPercent: 90,
+    weight: 66,
+    someType: someTypes.firstType,
   },
 ];
 
@@ -142,54 +144,57 @@ const SineDataTable = ({
     data={data}
     onFiltersChange={action('onFiltersChange')}
     order={{
-      id: 'weightForSorting',
+      id: 'weight',
       desc: true,
     }}
     language={language}
-    renderMobileTitle={(row) => row.original.bonusObject}
+    renderMobileTitle={(row) => row.original.employee}
     loading={loading}
     enableTableStatePersistance
     actions={actions}
     columns={[
       {
-        Header: 'Bonus Object',
-        accessor: 'bonusObject',
+        Header: 'Employee',
+        accessor: 'employee',
         // Use our custom `fuzzyText` filter on this column
         filter: 'fuzzyText',
-        Cell: ({ row }) => renderBonusObject(row),
-        Footer: () => <strong>Total</strong>,
+        Cell: ({ row }) => row.original.employee,
+        Footer: () => 'Total',
         nonMobileColumn: true,
         principalFilterableColumn: true,
       },
       {
         Header: 'Weight (%)',
-        accessor: 'weightForSorting',
-        width: 160,
+        accessor: 'weight',
+        width: 140,
         disableFilters: true,
         Footer: () => 100,
       },
       {
-        Header: 'Planned (₽)',
+        Header: 'Planned',
         accessor: 'plannedSales',
+        width: 200,
         disableFilters: true,
-        Footer: () => '48 529 957,27 ₽',
+        Footer: () => '48 529 957,27',
       },
       {
-        Header: 'Previous (₽)',
+        Header: 'Previous',
         accessor: 'previousSales',
+        width: 200,
         disableSortBy: true,
         disableFilters: true,
-        Footer: () => '42 238 542,94 ₽',
+        Footer: () => '42 238 542,94',
       },
       {
-        Header: 'Actual (₽)',
+        Header: 'Actual',
         accessor: 'actualSales',
+        width: 200,
         disableSortBy: true,
         disableFilters: true,
-        Footer: () => '47 193 196,2 ₽',
+        Footer: () => '47 193 196,2',
       },
       {
-        Header: 'Target (%)',
+        Header: 'Target',
         accessor: 'targetAchivementPercent',
         width: 100,
         disableSortBy: true,
@@ -204,17 +209,17 @@ const SineDataTable = ({
         minWidth: 140,
         id: 'newSlider',
         accessor: 'targetAchivementPercent',
-        Cell: () => (<input type="range" min={0} max={100} value={79} readOnly />),
+        Cell: () => (<input type="range" min={0} max={100} defaultValue={79} />),
         twoRowsMobileLayout: true,
         noFooterColumn: true,
       },
       {
-        Header: 'Bonus (₽)',
+        Header: 'Bonus',
         id: 'calculatedBonus',
         disableSortBy: true,
         disableFilters: true,
         accessor: (row) => row.forcastedBonus || row.calculatedBonus,
-        Cell: (row) => `${row.value} ₽`,
+        Cell: (row) => row.value,
         noFooterColumn: true,
       },
       {
@@ -230,11 +235,3 @@ const SineDataTable = ({
     ]}
   />
 );
-
-function renderBonusObject(row) {
-  return (
-    <b>
-      {row.original.bonusObject}
-    </b>
-  );
-}
